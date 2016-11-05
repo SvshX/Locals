3adb4470e7413fae2c11514842a7a1abc2228040
sash83 <saschamelcher@gmail.com>
2016-11-05 12:57:49 +0100

    first commit

---
+++ Yaknak/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Yaknak
+//
+//  Created by Sascha Melcher on 05/11/2016.
+//  Copyright © 2016 Locals Labs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
